18acbe697dd63cf4b65242b4b0c0bc5c7171ce31
sonaljain-05 <sonaljjainn@gmail.com>
2026-06-04 00:23:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ div[data-testid="stRadio"] label {
 </style>
 """, unsafe_allow_html=True)
 
-img = Image.open(r"E:\data science\medical prediction\picture\medical.png")
+img = Image.open("picture\medical.png")
 img = img.resize((500, 900))
 
 col1, col2 = st.columns(2)
@@ -27,23 +27,23 @@ with col1:
 with col2:
     st.markdown("<h1 style='color:#0ba8b0;'>Medical Prediction App</h1>", unsafe_allow_html=True)
 
-    st.image(r"E:\data science\medical prediction\picture\globe.png", width=50)
+    st.image("picture\globe.png", width=50)
 
     region = st.selectbox("Region", ["northeast", "northwest", "southeast", "southwest"])
 
-    st.image(r"E:\data science\medical prediction\picture\age.png", width=50)
+    st.image("picture\age.png", width=50)
     age = st.selectbox("Age", list(range(1, 101)), index=26)
 
-    st.image(r"E:\data science\medical prediction\picture\bmi.png", width=50)
+    st.image("picture\bmi.png", width=50)
     bmi = st.number_input("BMI", 10.0, 60.0, 24.0)
 
-    st.image(r"E:\data science\medical prediction\picture\children.png", width=50)
+    st.image("picture\children.png", width=50)
     children = st.number_input("Children", 0, 10, 0)
 
-    st.image(r"E:\data science\medical prediction\picture\sex.png", width=50)
+    st.image("picture\sex.png", width=50)
     sex = st.radio("Sex", ["male", "female"])
 
-    st.image(r"E:\data science\medical prediction\picture\smoker.png", width=50)
+    st.image("picture\smoker.png", width=50)
     smoker = st.radio("Smoker", ["yes", "no"])
 
     sex = 1 if sex == "male" else 0
